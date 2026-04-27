37a61e503f06101f6b51ec679747d1484195bfea
Pizza104 <167818115+Pizza104@users.noreply.github.com>
2026-04-27 11:05:43 -0300

    Atividade do dia 27/04
    Como pedido adicionei o Fornecedor, Cliente e também Conn. Todos os códigos do banco de dados estão no Procedure.txt

---
+++ Cliente.php
@@ -0,0 +1,50 @@
+<?php
+require_once 'Conn.php';
+
+class Cliente{
+
+    private $id;
+    private $nome;
+    private $email;
+
+    public function getId(){
+        return $this->id;
+    }
+
+    public function setId($id){
+        $this->id = $id;
+    }
+
+    public function getNome(){
+        return $this->nome;
+    }
+
+    public function setNome($nome){
+        $this->nome = $nome;
+    }
+
+    public function getEmail(){
+        return $this->email;
+    }
+
+    public function setEmail($email){
+        $this->email = $email;
+    }
+
+    public function salvar(){
+
+        $conn = new Conn();
+        $pdo = $conn->conectar();
+
+        $sql = "INSERT INTO cliente(nome,email)
+                VALUES(:nome,:email)";
+
+        $stmt = $pdo->prepare($sql);
+
+        $stmt->bindValue(':nome',$this->nome);
+        $stmt->bindValue(':email',$this->email);
+
+        return $stmt->execute();
+    }
+}
+?>
